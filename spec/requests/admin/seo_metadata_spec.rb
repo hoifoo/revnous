74a7aa89ad293643187d55b2881344f0bb48c8d5
Irfan Ahmed <irfan@hoifoo.com>
2025-12-11 16:36:14 +0100

    fix: lint and test coverages

--- spec/requests/admin/seo_metadata_spec.rb
+++ spec/requests/admin/seo_metadata_spec.rb
@@ -1,25 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe "Admin::SeoMetadata", type: :request do
+  let(:admin) { create(:user, :admin) }
+
+  before do
+    sign_in admin
+  end
+
   describe "GET /index" do
     it "returns http success" do
-      get "/admin/seo_metadata/index"
+      get admin_seo_metadata_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /new" do
     it "returns http success" do
-      get "/admin/seo_metadata/new"
+      get new_admin_seo_metadatum_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /edit" do
+    let!(:seo_metadatum) { SeoMetadatum.create!(page_identifier: "home#index", page_title: "Test") }
+
     it "returns http success" do
-      get "/admin/seo_metadata/edit"
+      get edit_admin_seo_metadatum_path(seo_metadatum)
       expect(response).to have_http_status(:success)
     end
   end
-
 end
