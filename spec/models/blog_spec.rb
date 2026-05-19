4b671cd9e07d45a8d929215c6fed7a25727aba60
Irfan Ahmed <irfan@hoifoo.com>
2026-05-19 22:53:58 +0200

    test(02-P3): add failing specs for img sanitizer round-trip and onerror strip
    - Example 3: preserves img tags with src, alt, and width attributes (ActiveStorage blob URL)
    - Example 4: strips onerror event-handler attribute while keeping img, src, and alt
    - These specs exercise the ALLOWED_ATTRIBUTES img coverage that was extended in P1/P2

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -23,5 +23,25 @@ RSpec.describe Blog, type: :model do
       expect(blog.body).to include('<table>')
       expect(blog.body).to include('<td>x</td>')
     end
+
+    it "preserves img tags with src, alt, and width attributes" do
+      body = '<p><img src="/rails/active_storage/blobs/redirect/xyz/photo.jpg" alt="A photo" width="480"></p>'
+      blog = build(:blog, body: body)
+      blog.save!
+      expect(blog.body).to include('<img')
+      expect(blog.body).to include('src="/rails/active_storage/blobs/redirect/xyz/photo.jpg"')
+      expect(blog.body).to include('alt="A photo"')
+      expect(blog.body).to include('width="480"')
+    end
+
+    it "strips onerror and other event-handler attributes on img tags" do
+      body = '<img src="x" onerror="alert(1)" alt="x">'
+      blog = build(:blog, body: body)
+      blog.save!
+      expect(blog.body).not_to include('onerror')
+      expect(blog.body).to include('<img')
+      expect(blog.body).to include('src="x"')
+      expect(blog.body).to include('alt="x"')
+    end
   end
 end
